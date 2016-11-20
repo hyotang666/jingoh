b51848efbceb817b191f6318892c6e64b6c20138
Shinichi Sato <hyotang.karakoma@gmail.com>
2016-11-21 04:08:41 +0900

    refactor: integrate almost same functions.
    Especially the-subject-procedure

--- org/src/miscellaneous.lisp
+++ org/src/miscellaneous.lisp
@@ -1,28 +1,21 @@
 (in-package :jingoh.org)
 
-(defun the-nil-subject-procedure(v org var body return)
-  `(LOOP :FOR (NIL . ,v) :ACROSS (!(ORG-SPECIFICATIONS ,org))
-	 :DO (MAP NIL (LAMBDA(,var),@body),v)
-	 :FINALLY(RETURN ,return)))
-
-(defun the-t-subject-procedure(var body org return)
-  `(BLOCK()
-     (MAP NIL (LAMBDA(,var),@body)
-	  (CDR(?!(FIND *SUBJECT* (!(ORG-SPECIFICATIONS ,org))
-		       :KEY #'CAR))))
-     ,return))
+(defun the-nil-subject-procedure(org var body return)
+  (let((v(gensym "V")))
+    `(LOOP :FOR (NIL . ,v) :ACROSS (!(ORG-SPECIFICATIONS ,org))
+	   :DO (MAP NIL (LAMBDA(,var)(TAGBODY ,@body)),v)
+	   :FINALLY(RETURN ,return))))
 
 (defun the-subject-procedure(var body gname org return)
   `(BLOCK()
-     (MAP NIL(LAMBDA(,var) ,@body)
+     (MAP NIL(LAMBDA(,var)(TAGBODY ,@body))
 	  (CDR(?!(FIND ,gname (!(ORG-SPECIFICATIONS ,org))
 		       :KEY #'CAR))))
      ,return))
 
 (defmacro do-requirements((var &optional(subject-designator T)(org '*org*) return)&body body)
   #.(Doc :jingoh.org "doc/do-requirements.M.md")
-  (let((gname(gensym "NAME"))
-       (v(gensym "V")))
+  (let((gname(gensym "NAME")))
     `(MACROLET((?!(form)
 		 `(OR ,form
 		      (ERROR'MISSING-SUBJECT :API 'DO-REQUIREMENTS
@@ -32,8 +25,8 @@
 		    ,FORM)))
        (LET((,gname ,subject-designator))
 	 (CASE,gname
-	   ((NIL),(the-nil-subject-procedure v org var body return))
-	   ((T),(the-t-subject-procedure var body org return))
+	   ((NIL),(the-nil-subject-procedure org var body return))
+	   ((T),(the-subject-procedure var body '*SUBJECT* org return))
 	   (OTHERWISE,(the-subject-procedure var body gname org return)))))))
 
 (define-compiler-macro do-requirements(&whole whole (var &optional(subject-designator T)(org '*org*) return)&body body)
@@ -47,8 +40,8 @@
 		 `(RESIGNAL-BIND((TYPE-ERROR()'NOT-ORG :API 'DO-REQUIREMENTS))
 		    ,FORM)))
        ,(case subject-designator
-	  ((NIL)(the-nil-subject-procedure(gensym "V")org var body return))
-	  ((T)(the-t-subject-procedure var body org return))
+	  ((NIL)(the-nil-subject-procedure org var body return))
+	  ((T)(the-subject-procedure var body '*SUBJECT* org return))
 	  (otherwise
 	    (the-subject-procedure var body subject-designator org return))))))
 
