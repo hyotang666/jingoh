03c04e24046cd48e88f6f4cd02f33160b25602da
Shinichi Sato <hyotang.karakoma@gmail.com>
2017-02-23 06:17:53 +0900

    fix: add-requirement

--- org/src/miscellaneous.lisp
+++ org/src/miscellaneous.lisp
@@ -80,17 +80,16 @@
 
   (defun add-requirement(subject requirement &optional(org *org*))
     #.(Doc :jingoh.org "doc/add-requirement.F.md")
-    (let((subject (find subject (! 1 (org-specifications org))
+    (let((sub (find subject (! 1 (org-specifications org))
 				    :key #'car)))
-      (if subject
-	(vector-push-extend requirement (cdr subject))
-	(dolist(subject *subjects*)
-	  (vector-push-extend (cons subject
-				    (make-array 1
-						:fill-pointer 1
-						:adjustable t
-						:initial-contents (list requirement)))
-			      (org-specifications org)))))
+      (if sub
+	(vector-push-extend requirement (cdr sub))
+	(vector-push-extend (cons subject
+				  (make-array 1
+					      :fill-pointer 1
+					      :adjustable t
+					      :initial-contents (list requirement)))
+			    (org-specifications org))))
     requirement)
 
   (defun org-requirements-count(org)
