dba0fab37dcddd42d75aa0693b0ebef26e2ca742
Shinichi Sato <hyotang.karakoma@gmail.com>
2017-03-24 00:28:08 +0900

    add: spec files, and fix some minor bugs.

--- documentizer/src/about-symbols.lisp
+++ documentizer/src/about-symbols.lisp
@@ -49,8 +49,8 @@
 	       (write-string "### ")
 	       (write-line (escape-* elt) *standard-output* :start 2))
 	      ((uiop:string-prefix-p "#+signature" elt)
-	       (format t  "### ")
-	       (write-line (escape-* elt) *standard-output* :start 11))
+	       (write-string "### ")
+	       (write-line (escape-* elt) *standard-output* :start 12))
 	      ((uiop:string-prefix-p "#| " elt)
 	       (write-string "# ")
 	       (write-line elt *standard-output* 
