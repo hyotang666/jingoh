204418a9eec006dc6dd81a4779bcfb30416381cb
Shinichi Sato <hyotang.karakoma@gmail.com>
2019-01-03 11:36:45 +0900

    Improve: JINGOH.GENERATOR:GENERATE((symbol symbol) ...)
    Old:	Does not have `APPEND` keyword parameter.
    Issue:	We must use ((system asdf:system)...) directly to append.
    New:	Improved.

--- generator/src/symbol.lisp
+++ generator/src/symbol.lisp
@@ -1,10 +1,10 @@
 (in-package :jingoh.generator)
 
-(defmethod generate ((symbol symbol) &key system init pathname)
+(defmethod generate ((symbol symbol) &key system init pathname append)
   (if(keywordp symbol)
     (if init
       (generate 'init :system symbol :pathname pathname)
-      (generate(asdf:find-system symbol)))
+      (generate(asdf:find-system symbol):append append))
     (let*((*package* (symbol-package symbol))
 	  (package-name(package-name *package*))
 	  (system(asdf:find-system (or system (string-downcase package-name))))
