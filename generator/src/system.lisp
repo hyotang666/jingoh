f7afa89c969a551bdc24e9fb1dcc40fe40fb0043
Shinichi Sato <hyotang.karakoma@gmail.com>
2019-08-23 18:19:35 +0900

    Fix: JINGOH.GENERATOR::%ADD-METHOD-EXTENSION
    Old:	Not support documentation importer.
    New:	Supported.

--- generator/src/system.lisp
+++ generator/src/system.lisp
@@ -39,24 +39,46 @@
 
 (defun %add-method-extension(name)
   (let((*package*(find-package :asdf)))
-    (format t "~%;; These two methods below are added by JINGOH.GENERATOR.~%~(~S~)~%~(~S~)~%~(~S~)"
-	    `(in-package :asdf)
-	    `(defmethod asdf:component-depends-on((asdf::o asdf:test-op)(asdf::c (eql (asdf:find-system ,name))))
-	       (append (call-next-method) '((asdf:test-op ,(test-name name)))))
-	    `(defmethod asdf:operate :around ((asdf::o asdf:test-op)(asdf::c (eql (asdf:find-system ,name)))
-					      &rest asdf::keys
-					      &key ((:compile-print *compile-print*))
-					      ((:compile-verbose *compile-verbose*))
-					      &allow-other-keys)
-	       (flet((asdf::jingoh.args(asdf::keys)
-		       (loop :for (asdf::key asdf::value) :on asdf::keys :by #'cddr
-			     :when (find asdf::key '(:on-fails :subject :vivid) :test #'eq)
-			     :collect asdf::key :and :collect asdf::value
-			     :else :when (eq :jingoh.verbose asdf::key)
-			     :collect :verbose :and :collect asdf::value)))
-		 (let((asdf::args(asdf::jingoh.args asdf::keys)))
-		   (declare(special asdf::args))
-		   (call-next-method)))))))
+    (format t "~%;; These forms below are added by JINGOH.GENERATOR.~{~%~(~S~)~}"
+	    `((in-package :asdf)
+	      (defmethod asdf:component-depends-on((asdf::o asdf:test-op)(asdf::c (eql (asdf:find-system ,name))))
+		(append (call-next-method) '((asdf:test-op ,(test-name name)))))
+	      (defmethod asdf:operate :around ((asdf::o asdf:test-op)(asdf::c (eql (asdf:find-system ,name)))
+					       &rest asdf::keys
+					       &key ((:compile-print *compile-print*))
+					       ((:compile-verbose *compile-verbose*))
+					       &allow-other-keys)
+		(flet((asdf::jingoh.args(asdf::keys)
+			(loop :for (asdf::key asdf::value) :on asdf::keys :by #'cddr
+			      :when (find asdf::key '(:on-fails :subject :vivid) :test #'eq)
+			      :collect asdf::key :and :collect asdf::value
+			      :else :when (eq :jingoh.verbose asdf::key)
+			      :collect :verbose :and :collect asdf::value)))
+		  (let((asdf::args(asdf::jingoh.args asdf::keys)))
+		    (declare(special asdf::args))
+		    (call-next-method))))
+	      (let((asdf::system
+		     (asdf:find-system "jingoh.documentizer" nil)))
+		(when asdf::system
+		  (asdf:load-system asdf::system)
+		  (defmethod asdf:operate :around ((asdf::o asdf:load-op)
+						   (asdf::c (eql (asdf:find-system ,name)))
+						   &key)
+		    (if(not(find-package "JINGOH.DOCUMENTIZER"))
+		      (call-next-method)
+		      (let*((asdf::forms nil)
+			    (*macroexpand-hook*
+			      (let((asdf::outer-hook *macroexpand-hook*))
+				(lambda(asdf::expander asdf::form asdf::env)
+				  (when(typep asdf::form '(cons (eql defpackage)*))
+				    (push asdf::form asdf::forms))
+				  (funcall asdf::outer-hook asdf::expander asdf::form asdf::env))))
+			    (*default-pathname-defaults*
+			      (merge-pathnames "spec/"
+					       (asdf:system-source-directory asdf::c))))
+			(multiple-value-prog1(call-next-method)
+			  (mapc (find-symbol "IMPORTER" "JINGOH.DOCUMENTIZER")
+				asdf::forms)))))))))))
 
 (defun spec-directory(system)
   (uiop:subpathname (asdf:system-source-directory (asdf:find-system system))
