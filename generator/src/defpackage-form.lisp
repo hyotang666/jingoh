972da13a5124abe71a7f63698e220b59e764b6ff
Shinichi Sato <hyotang.karakoma@gmail.com>
2019-10-30 00:39:26 +0900

    Refactor: JINGOH.GENERATOR:GENERATE((form list) &key append)
    New:	Replace MACROLET.

--- generator/src/defpackage-form.lisp
+++ generator/src/defpackage-form.lisp
@@ -2,26 +2,26 @@
 
 (defmethod generate((form list) &key append)
   (assert(typep form '(CONS (EQL DEFPACKAGE) T)))
-  (macrolet((expand(existp)
-	      `(UIOP:WITH-OUTPUT-FILE(*STANDARD-OUTPUT* PATH
-							,@(when existp
-							    `(:IF-EXISTS :APPEND)))
-		 ,@(unless existp
-		     `((GENERATE-HEADER(SECOND FORM))))
-		 (DOLIST(SYMBOL (EXPORTS FORM))
-		   (SYMBOL-GENERATE SYMBOL (SECOND FORM))))))
-    (labels((exports(form)
-	      (loop :for elt :in form
-		    :when (typep elt '(CONS(EQL :EXPORT)T))
-		    :append (cdr elt))))
-      (let((path
-	     (Path-of (string-downcase(second form))
-		      "lisp"))
-	   (*package*(find-package(second form))))
-	(if (probe-file path)
-	  (when append
-	    (expand t))
-	  (expand nil))))))
+  (labels((exports(form)
+	    (loop :for elt :in form
+		  :when (typep elt '(CONS(EQL :EXPORT)T))
+		  :append (cdr elt))))
+    (let*((path
+	    (Path-of (string-downcase(second form))
+		     "lisp"))
+	  (*package*(find-package(second form)))
+	  (existp
+	    (probe-file path)))
+      (uiop:with-output-file(*standard-output* path
+					       :if-exists
+					       (if (and existp
+							append)
+						 :append
+						 :error))
+	(unless existp
+	  (generate-header (second form)))
+	(dolist(symbol (exports form))
+	  (symbol-generate symbol (second form)))))))
 
 (defun generate-header(package-name)
   (let((spec-name(intern (format nil "~A.SPEC"package-name)
