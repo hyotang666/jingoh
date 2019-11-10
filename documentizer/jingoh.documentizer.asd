9c5ec63a572283d6ec26c10ba18050fe40a5e217
Shinichi Sato <hyotang.karakoma@gmail.com>
2019-11-10 10:26:45 +0900

    Refactor: Split JINGOH.DOCUMENTIZER::SECTION-P to easy debugging.

--- documentizer/jingoh.documentizer.asd
+++ documentizer/jingoh.documentizer.asd
@@ -1,13 +1,14 @@
 ; vim: ft=lisp et
 (in-package :asdf)
 (defsystem :jingoh.documentizer
-  :version "3.7.2"
+  :version "3.8.0"
   :depends-on (
                "3bmd" ; markdown.
                "3bmd-ext-code-blocks"
                "read-as-string" ; s-expression string reader.
                "uiop" ; utilities.
                "prompt-for" ; Type safe user input.
+               "null-package" ; Safe S expression reader.
                )
   :pathname "src/"
   :components(;; package.
--- documentizer/src/parse-spec.lisp
+++ documentizer/src/parse-spec.lisp
@@ -12,39 +12,41 @@
 	  :collect exp)))
 
 (defun sectionize(list)
-  (flet((section-p(elt)
-	  (and (char= #\( (char elt 0))
-	       (let*((*package*
-		       (find-package :jingoh.documentizer))
-		     (sexp
-		       (read-from-string elt)))
-		 (when(find (car sexp)
-			    '(requirements-about common-requirements-about))
-		   sexp)))))
-    (loop :for list :on list
-	  :with body
-	  :for section = (section-p (car list))
-	  :for alias = (getf section :as)
-	  :if section
-	  :do (setf body (subseq list 1
-				 (position-if #'section-p list
-					      :start 1)))
-	  :and :if alias
-	  :collect (Make-common :names (cadr section)
-				:alias alias
-				:doc-type (getf section :doc-type)
-				:path (Target-path(symbol-name(gensym"C_")))
-				:body body)
-	  :into commons
-	  :else :collect(Make-single
-			  :name (cadr section)
-			  :doc-type (getf section :doc-type)
-			  :path (Target-path
-				  (format nil "S_~A"
-					  (replace-invalid-chars (cadr section))))
-			  :body body)
-	  :into singles
-	  :finally (return(values singles commons)))))
+  (loop :for list :on list
+	:with body
+	:for section = (section-p (car list))
+	:for alias = (getf section :as)
+	:if section
+	:do (setf body (subseq list 1
+			       (position-if #'section-p list
+					    :start 1)))
+	:and :if alias
+	:collect (Make-common :names (cadr section)
+			      :alias alias
+			      :doc-type (getf section :doc-type)
+			      :path (Target-path(symbol-name(gensym"C_")))
+			      :body body)
+	:into commons
+	:else :collect(Make-single
+			:name (cadr section)
+			:doc-type (getf section :doc-type)
+			:path (Target-path
+				(format nil "S_~A"
+					(replace-invalid-chars (cadr section))))
+			:body body)
+	:into singles
+	:finally (return(values singles commons))))
+
+(defun section-p(elt)
+  (and (char= #\( (char elt 0))
+       (let*((*package*
+	       (find-package :jingoh.documentizer))
+	     (sexp
+	       (with-input-from-string(s elt)
+		 (null-package:read-with-null-package s))))
+	 (when(find (car sexp)
+		    '(requirements-about common-requirements-about))
+	   sexp))))
 
 (defun replace-invalid-chars(arg)
   (loop :for c :across (string-downcase(string arg))
